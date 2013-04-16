e6f37209448e0a4feee2acc1d7aabf060c271925
Bryan Berry <bryan.berry@gmail.com>
2013-04-16 16:24:54 +0300

    better set the ip for vagrant

--- files/default/plugins/vagrant-net.rb
+++ files/default/plugins/vagrant-net.rb
@@ -1,10 +1,18 @@
-
 # borrowed from https://gist.github.com/2050259
 
 provide "ipaddress"
 require_plugin "#{os}::network"
-network["interfaces"]["eth1"]["addresses"].each do |ip, params|
-  if params['family'] == ('inet')
-    ipaddress ip
+require_plugin "#{os}::virtualization"
+require_plugin "passwd"
+
+if virtualization["system"] == "vbox"
+  if etc["passwd"].any? { |k,v| k == "vagrant"}
+    if network["interfaces"]["eth1"]
+      network["interfaces"]["eth1"]["addresses"].each do |ip, params|
+        if params['family'] == ('inet')
+          ipaddress ip
+        end
+      end
+    end
   end
 end
